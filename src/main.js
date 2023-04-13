e595422ce440c71139c4b7e7b040e3fc4af08c18
NelloRossi <rossianiello97@gmail.com>
2023-04-13 14:21:26 +0200

    scaffolding

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+
+import 'bootstrap/scss/bootstrap.scss';
+
+import './assets/general.scss'
+
+createApp(App).mount('#app')
