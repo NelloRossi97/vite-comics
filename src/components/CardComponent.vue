bf1a490bef916e9d003cd94756f0b06955902c1b
NelloRossi <rossianiello97@gmail.com>
2023-04-14 14:44:36 +0200

    added component card in the main

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex flex-column align-items-center">
+        <img class="img-fluid mb-2" :src="thumb" :alt="series">
+        <h6 class="text-uppercase">{{ series }}</h6>
+        <p>Comprala a {{ price }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'CardComponent',
+        props: ['thumb', 'series', 'price']
+    }
+</script>
+
+<style lang="scss" scoped>
+img{
+    width: 200px;
+    height: 300px;
+}
+p{
+    font-size: 1rem;
+    font-weight: 100;
+}
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,11 +2,7 @@
     <main>
         <div class="container py-5 fw-bold fs-3">
             <div class="row">
-                <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 d-flex flex-column align-items-center" v-for="(item, index) in seriesData" :key="index">
-                    <img class="img-fluid mb-2" :src="item.thumb" :alt="item.series">
-                    <h6 class="text-uppercase">{{ item.series }}</h6>
-                    <p>Comprala a {{ item.price }}</p>
-                </div>
+                <CardComponent :thumb="item.thumb" :series="item.series" :price="item.price" v-for="item in seriesData"/>
             </div>
         </div>
         <CallToActionComponent/>
@@ -16,6 +12,7 @@
 <script>
 import CallToActionComponent from './CallToActionComponent.vue';
 import { comicsData } from '../data/data';
+import CardComponent from './CardComponent.vue';
     export default {
         name: 'MainComponent',
         data(){
@@ -24,18 +21,12 @@ import { comicsData } from '../data/data';
             }
         },
         components: {
-            CallToActionComponent
+            CallToActionComponent,
+            CardComponent
         }
     }
 </script>
 
 <style lang="scss" scoped>
-img{
-    width: 200px;
-    height: 300px;
-}
-p{
-    font-size: 1rem;
-    font-weight: 100;
-}
+
 </style>
