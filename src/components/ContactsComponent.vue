6f9816c26bfc0e910690f52682247a26e5734d16
NelloRossi <rossianiello97@gmail.com>
2023-04-14 16:05:16 +0200

    fixed media query contacts component

--- src/components/ContactsComponent.vue
+++ src/components/ContactsComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="contacts">
         <div class="container py-4 d-flex justify-content-between align-items-center">
-            <button class="text-uppercase text-white px-3 fw-bold">sign-up now!</button>
+            <button class="text-uppercase text-white px-3 fw-bold me-1 fs-7">sign-up now!</button>
             <div class="d-flex gap-3 align-items-center">
-                <h4 class="text-uppercase">follow us</h4>
+                <h4 class="text-uppercase d-none d-md-flex">follow us</h4>
                 <img src="/images/footer-facebook.png" alt="Facebook">
                 <img src="/images/footer-twitter.png" alt="Twitter">
                 <img src="/images/footer-youtube.png" alt="Youtube">
@@ -27,12 +27,22 @@ button{
     background-color: transparent;
     border: 2px solid $blue-secondary;
     height: 50px;
+    font-size: 0.6rem;
 }
 h4{
     color: $blue-secondary;
 }
 img{
-    width: 50px;
-    height: 50px;
+    width: 25px;
+    height: 25px;
+}
+@media screen and (min-width: 461px) {
+    button{
+        font-size: 1rem;
+    }
+    img{
+        width: 50px;
+        height: 50px;
+    }
 }
 </style>
